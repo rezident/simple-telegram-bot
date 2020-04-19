c00886fb8cbb56e7256341c70818a63bdee4fa3e
Yuri Nazarenko <m@rezident.org>
2020-04-19 16:07:59 +0300

    Add getting file content

--- src/types/File.php
+++ src/types/File.php
@@ -2,6 +2,8 @@
 
 namespace TelegramBot\types;
 
+use TelegramBot\TelegramBot;
+
 /**
  * This object represents a file ready to be downloaded. The file can be downloaded via the link https://api.telegram.org/file/bot<token>/<file_path>. It is guaranteed that the link will be valid for at least 1 hour. When the link expires, a new one can be requested by calling getFile.
  *
@@ -23,5 +25,21 @@ namespace TelegramBot\types;
  */
 class File extends AbstractType
 {
+    const DOWNLOAD_URL_PREFIX = 'https://api.telegram.org/file/bot';
 
+    /**
+     * Downloads a file, represented by this type
+     *
+     * @param TelegramBot $bot
+     *
+     * @return false|string
+     *
+     * @author Yuri Nazarenko / rezident <m@rezident.org>
+     */
+    public function getFileContent(TelegramBot $bot)
+    {
+        $url = implode('', [self::DOWNLOAD_URL_PREFIX, $bot->getToken(), '/' . $this->getFilePath()]);
+
+        return file_get_contents($url);
+    }
 }
